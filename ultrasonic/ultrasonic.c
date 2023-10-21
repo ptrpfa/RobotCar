2ec33bd3ff1abfdd4a39e28b2891ce3a4efe547a
lionel-sim <lionelsimweixian@gmail.com>
2023-10-21 12:19:16 +0800

    Ultrasonic Kalman filter WIP

--- ultrasonic/ultrasonic.c
+++ ultrasonic/ultrasonic.c
@@ -1,12 +1,44 @@
 // Get readings from ultrasonic sensor
 
-#include "pico/stdlib.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include "pico/stdlib.h"
 #include "hardware/gpio.h"
 #include "hardware/timer.h"
 #include "ultrasonic.h"
 
 int timeout = 26100;
+
+typedef struct kalman_state_
+{
+    double q; // process noise covariance
+    double r; // measurement noise covariance
+    double x; // estimated value
+    double p; // estimation error covariance
+    double k; // kalman gain
+} kalman_state;
+
+static kalman_state *kalman_init(double q, double r, double p, double initial_value)
+{
+    kalman_state *state = calloc(1, sizeof(kalman_state));
+    state->q = q;
+    state->r = r;
+    state->p = p;
+    state->x = initial_value;
+
+    return state;
+}
+
+void kalman_update(kalman_state *state, double measurement)
+{
+    // Prediction update
+    state->p = state->p + state->q;
+
+    // Measurement update
+    state->k = state->p / (state->p + state->r);
+    state->x = state->x + state->k * (measurement - state->x);
+    state->p = (1 - state->k) * state->p;
+}
 
 void setupUltrasonicPins()
 {
@@ -39,8 +71,29 @@ uint64_t getPulse()
     return absolute_time_diff_us(startTime, endTime);
 }
 
-uint64_t getCm()
+double getCm(kalman_state *state)
 {
     uint64_t pulseLength = getPulse(TRIGPIN, ECHOPIN);
-    return pulseLength / 29 / 2;
+    double measured = pulseLength / 29.0 / 2.0;
+    kalman_update(state, measured);
+
+    return state->x;
+}
+
+int main()
+{
+    // Driver code to run ultrasonic sensor
+    double cm;
+    stdio_init_all();
+    setupUltrasonicPins();
+    kalman_state *state = kalman_init(0.125, 32, 1023, 0);
+    while (true)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            cm = getCm(state);
+        }
+        printf("Distance: %.2lf\n", cm);
+        sleep_ms(500);
+    }
 }
--- ultrasonic/ultrasonic.h
+++ ultrasonic/ultrasonic.h
@@ -1,9 +1,20 @@
 #ifndef ultrasonic_h
 #define ultrasonic_h
 
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "hardware/timer.h"
+
 #define TRIGPIN 0
 #define ECHOPIN 1
 
+typedef struct kalman_state_ kalman_state;
+
+static kalman_state *kalman_init(double q, double r, double p, double initial_value);
+void kalman_update(kalman_state *state, double measurement);
 void setupUltrasonicPins();
-uint64_t getCm();
+uint64_t getPulse();
+double getCm(kalman_state *state);
+
 #endif
