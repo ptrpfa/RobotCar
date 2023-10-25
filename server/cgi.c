cbe49936f5f5ddeb3112dc9731d85eb7967ce56a
dinieaziz <nurdinieaziz@gmail.com>
2023-10-26 00:18:34 +0800

    server driver file cleanup

---
+++ server/cgi.c
@@ -0,0 +1,54 @@
+#include "cgi.h"
+
+// CGI handler which is run when a request for /led.cgi is detected
+const char * cgi_led_handler(int iIndex, int iNumParams, char *pcParam[], char *pcValue[])
+{
+    // Check if an request for LED has been made (/led.cgi?led=x)
+    if (strcmp(pcParam[0] , "led") == 0){
+        // Look at the argument to check if LED is to be turned on (x=1) or off (x=0)
+        if(strcmp(pcValue[0], "0") == 0)
+            cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+        else if(strcmp(pcValue[0], "1") == 0)
+            cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+    }
+    
+    // Send the index page back to the user
+    return "/index.shtml";
+}
+
+// CGI handler which is run when a request for /engine.cgi is detected
+const char * cgi_engine_handler(int iIndex, int iNumParams, char *pcParam[], char *pcValue[])
+{
+    // Check if a request for the engine has been made (/engine.cgi?engine=x)
+    if (strcmp(pcParam[0], "engine") == 0) {
+        // Look at the argument to check if the engine is to be started (x=1) or stopped (x=0)
+        if (strcmp(pcValue[0], "0") == 0) {
+            // Stop the motor
+            startCar = 0;
+        } else if (strcmp(pcValue[0], "1") == 0) {
+            // Start the motor (you can replace 1.0f with the desired PWM value)
+            startCar = 1;
+        }
+    }
+
+    // Send the index page back to the user
+    return "/index.shtml";
+}
+
+// tCGI Struct
+// Fill this with all of the CGI requests and their respective handlers
+static const tCGI cgi_handlers[] = {
+    {
+        // Html request for "/led.cgi" triggers cgi_led_handler
+        "/led.cgi", cgi_led_handler
+    },
+    {
+        // Html request for "/engine.cgi" triggers cgi_engine_handler
+        "/engine.cgi", cgi_engine_handler
+    },
+};
+
+void cgi_init(void)
+{
+    http_set_cgi_handlers(cgi_handlers, LWIP_ARRAYSIZE(cgi_handlers));
+}
---
+++ server/server.c
@@ -0,0 +1,90 @@
+#include "lwip/apps/httpd.h"
+#include "pico/stdlib.h"
+#include "pico/cyw43_arch.h"
+#include "lwipopts.h"
+#include "ssi.h"
+#include "cgi.h"
+#include "motor.h"
+#include "ultrasonic.h"
+
+int startCar = 0;
+
+// WIFI Credentials
+const char WIFI_SSID[] = "SINGTEL-WV58";
+const char WIFI_PASSWORD[] = "7tn83dwfae";
+
+/*
+int main() {
+    stdio_init_all();
+
+    cyw43_arch_init();
+    cyw43_arch_enable_sta_mode();
+
+    // Connect to the WiFI network - loop until connected
+    while(cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASSWORD, CYW43_AUTH_WPA2_AES_PSK, 10000) != 0){
+        printf("Attempting to connect...\n");
+    }
+
+    // Print a success message once connected
+    printf("Connected! \n");
+
+    // Initialise Web Server, SSI and CGI handler
+    httpd_init();
+    printf("Http server initialised\n");
+    ssi_init(); 
+    printf("SSI Handler initialised\n");
+    cgi_init();
+    printf("CGI Handler initialised\n");
+
+    // Initialise motor GPIO pins and motor PWM
+    initMotorSetup();
+    initMotorPWM();
+
+    // Initialise encoder GPIO pins
+    initEncoderSetup();
+
+    // Initialise ultrasonic sensor
+    setupUltrasonicPins();
+    kalman_state *state = kalman_init(1, 100, 0, 0);
+    sleep_ms(1000);
+
+    double cm;
+
+    while (1) {
+        if (startCar == 1) {
+            // Get distance from ultrasonic sensor
+            for (int i = 0; i < 10; i++) {
+                cm = getCm(state);
+            }
+
+            printf("Distance: %.2lf cm\n", cm);
+
+            // If an obstacle is too close, stop motor and turn left
+            if (cm < 22) {            
+                // // Get distance from ultrasonic sensor
+                // for (int i = 0; i < 10; i++) {
+                //     cm = getCm(state);
+                // }
+
+                // if (cm < 10) {
+                stopMotor();
+                sleep_ms(1000);
+                turnMotor(1);
+                // }
+            } 
+            else {
+                // Run the motor at half of the duty cycle
+                // moveMotor(1563);
+                moveMotor(3125);
+            }
+    
+            sleep_ms(250);
+        }
+        else {
+            stopMotor();
+        }
+    }
+
+    return 0;
+}
+*/
