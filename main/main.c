8261aaf1e6246da11b44fde00a3a75fac6261633
dinieaziz <nurdinieaziz@gmail.com>
2023-10-26 00:21:32 +0800

    server and main fusion

--- main/main.c
+++ main/main.c
@@ -3,88 +3,85 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "pico/stdlib.h"
+#include "pico/cyw43_arch.h"
 #include "hardware/adc.h"
 #include "ultrasonic.h"
 #include "motor.h"
+#include "ssi.h"
+#include "cgi.h"
 
-// Read temperature (temporary function)
-float read_onboard_temperature()
-{
-    /* 12-bit conversion, assume max value == ADC_VREF == 3.3 V */
-    const float conversionFactor = 3.3f / (1 << 12);
+int startCar = 0;
 
-    float adc = (float)adc_read() * conversionFactor;
-    float tempC = 27.0f - (adc - 0.706f) / 0.001721f;
+// WIFI Credentials
+const char WIFI_SSID[] = "";        // Insert own WIFI SSID
+const char WIFI_PASSWORD[] = "";    // Insert own WIFI password
 
-    return tempC;
-}
-
-int main()
-{
-    /*
+int main() {
     stdio_init_all();
-    // Sensor values
-    float temperature = 0.0;
-    uint64_t cm = 0.0;
 
-    // Init pins
-    adc_init();
-    adc_set_temp_sensor_enabled(true);
-    adc_select_input(4);
+    cyw43_arch_init();
+    cyw43_arch_enable_sta_mode();
 
-    setupUltrasonicPins();
-
-    // Run functions here
-
-    // Temporary while loop to publish every one second
-    while (true)
-    {
-        // Publish message
-
-        // // Get temperature value and publish
-        // temperature = read_onboard_temperature();
-        // sprintf(temp_message, "%.2f", temperature);
-
-        // // Get ultrasonic value and publish
-        // cm = getCm();
-        // sprintf(ultrasonic_message, "%llu", cm);
+    // Connect to the WiFI network - loop until connected
+    while(cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASSWORD, CYW43_AUTH_WPA2_AES_PSK, 10000) != 0){
+        printf("Attempting to connect...\n");
     }
-    return 0;
-    */
 
-    stdio_init_all();
+    // Print a success message once connected
+    printf("Connected!\n");
 
-    // Initialise motor GPIO pins
+    // Initialise web server
+    httpd_init();
+    printf("Http server initialised\n");
+
+    // Initialise SSI and CGI handler
+    ssi_init(); 
+    printf("SSI Handler initialised\n");
+    cgi_init();
+    printf("CGI Handler initialised\n");
+
+    // Initialise motor GPIO pins and motor PWM
     initMotorSetup();
-
-    // Initialise motor PWM
     initMotorPWM();
+    printf("Motor pins and PWM initialised\n");
+
+    // Initialise encoder GPIO pins
+    initEncoderSetup();
+    printf("Wheel encoder pins initialised\n");
 
     // Initialise ultrasonic sensor
     setupUltrasonicPins();
-    kalman_state *state = kalman_init(0.125, 32, 1023, 0);
+    kalman_state *state = kalman_init(1, 100, 0, 0);
+    sleep_ms(1000);
 
     double cm;
 
     while (1) {
-        // Get distance from ultrasonic sensor
-        for (int i = 0; i < 10; i++) {
-            cm = getCm(state);
+        if (startCar == 1) {
+            // Get distance from ultrasonic sensor
+            for (int i = 0; i < 10; i++) {
+                cm = getCm(state);
+            }
+
+            printf("Distance: %.2lf cm\n", cm);
+
+            // If an obstacle is too close, stop motor and turn left
+            if (cm < 22) {            
+                stopMotor();
+                sleep_ms(1000);
+                turnMotor(1);
+            } 
+            else {
+                // Run the motor at half of the duty cycle
+                // moveMotor(1563);
+                moveMotor(3125);
+            }
+    
+            sleep_ms(250);
         }
-
-        printf("Distance: %.2lf cm\n", cm);
-
-        // If an obstacle is too close, stop motor and turn left
-        if (cm < 20) {
-            stopMotor();
-            turnMotor(1);
-        } 
         else {
-            // Run the motor at half of the duty cycle
-            moveMotor(1563);
+            stopMotor();
         }
-        
-        sleep_ms(500);
     }
 
     return 0;
