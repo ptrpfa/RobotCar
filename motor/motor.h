97435c14f512330d7788cb1a8b5e7bcc2a2b92cb
JRRZZ <Jianrong12@outlook.com>
2023-11-22 15:02:40 +0800

    CAR BOMB

--- motor/motor.h
+++ motor/motor.h
@@ -13,12 +13,18 @@
 #define R_MOTOR_IN3 4  // GPIO pin for R motor input 1
 #define R_MOTOR_IN4 3  // GPIO pin for R motor input 2
 #define R_MOTOR_ENB 2  // GPIO pin for R motor enable
+#define PWM_MIN 1600
+#define PWM_MAX 3125
+
+extern volatile float pwmL;
+extern volatile float pwmR;
 
 // Functions for motors
 void initMotorSetup();
 void initMotorPWM();
-void moveMotor(float pwm);
+void moveMotor(float pwmL , float pwmR);
 void stopMotor();
 void turnMotor(int direction);
+void update_motor_speed();
 
 #endif
